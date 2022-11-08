f9baa3c74d172675d88fcb383f37ad2c358bc952
siddumbre <siddharth.dumbre@somaiya.edu>
2022-11-08 21:22:35 +0530

    split input

---
+++ dxfx.py
@@ -0,0 +1,7 @@
+d=list(map(int,input().split(' ')))
+rad=d[0]
+deg=list(map(int,input().split(' ')))
+vel=list(map(int,input().split(' ')))
+direction=list(map(int,input().split(' ')))
+time=int(input())
+pos=list()
